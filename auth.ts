faf8ce3ff5fc8e7d6e0d3ba19b01ed36d0959793
branchingoutloud <pranav.sonawane@pyrack.com>
2025-05-22 19:05:43 +0530

    added basic nodemailer setup

--- auth.ts
+++ auth.ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import type { Provider } from "next-auth/providers";
+import Nodemailer from "next-auth/providers/nodemailer";
 
-const providers: Provider[] = [Google];
+const providers: Provider[] = [
+  Google,
+  Nodemailer({
+    server: process.env.EMAIL_SERVER,
+    from: process.env.EMAIL_FROM,
+  }),
+];
 
 export const providerMap = providers
   .map((provider) => {
