6e9d68e8261e335533de0b6e1dd4520b195cefa5
snowrlax <sonawanepranav19@gmail.com>
2025-07-07 21:35:39 +0530

    fixed api module error

--- app/api/shops/[shopId]/customers/[id]/route.ts
+++ app/api/shops/[shopId]/customers/[id]/route.ts
@@ -1,2 +1,7 @@
 // GET: Get a specific customer, PUT: Update a specific customer, DELETE: Delete a specific customer
 
+import { NextResponse } from "next/server";
+
+export async function GET () {
+    return NextResponse.json({})
+}
--- app/api/shops/[shopId]/customers/search/route.ts
+++ app/api/shops/[shopId]/customers/search/route.ts
@@ -1,1 +1,7 @@
 // POST: get the user based on their phone number, etc.
+
+import { NextResponse } from "next/server";
+
+export async function GET () {
+    return NextResponse.json({})
+}
